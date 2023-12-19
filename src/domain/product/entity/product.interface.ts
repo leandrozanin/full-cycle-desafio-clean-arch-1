39ca2f60b7fc3cdddc701f65abacc417ff85f9b7
Leandro Zanin <sr.leandrozanin@gmail.com>
2023-12-19 20:19:07 -0300

    projeto aula

---
+++ src/domain/product/entity/product.interface.ts
@@ -0,0 +1,8 @@
+export default interface ProductInterface {
+  get id(): string;
+  get name(): string;
+  get price(): number;
+
+  changePrice(price: number): void
+  changeName(name: string): void;
+}
---
+++ src/usecase/product/create/create.product.integration.spec.ts
@@ -0,0 +1,49 @@
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import CreateProductUseCase from "./create.product.usecase";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import { Sequelize } from "sequelize-typescript";
+
+
+const input = {
+    type: "a",
+    name: "Product A",
+    price: 134.54
+};
+
+describe("Unit test create product use case", () => {
+
+    let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([
+      ProductModel,
+    ]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+
+    it("should create a product", async () => {
+        const productRepository = new ProductRepository();
+        const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+        await productCreateUseCase.execute(input);
+        const output = await productCreateUseCase.execute(input);
+
+        expect(output).toEqual({
+            id: expect.any(String),
+            name: input.name,
+            price: input.price
+        });
+    })
+})
